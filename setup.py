c67de4f3e298c8158cbc12500066bdf218bda38e
Oleg Churkin <bahusoff@gmail.com>
2018-07-26 00:00:08 +0300

    Docs fixed, version bump.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def get_long_description():
 setup(
     name='uwsgi-tasks',
     packages=['uwsgi_tasks'],
-    version='0.7.0',
+    version='0.7.1',
     description='Asynchronous tasks management with UWSGI server',
     author='Oleg Churkin',
     author_email='bahusoff@gmail.com',
